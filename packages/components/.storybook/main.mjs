3e4772015cc042d6edd8fd83f685eaacef3d889c
Christian Llontop <chrisllontop@icloud.com>
2024-02-19 20:19:13 -0500

    move to use vite as bundler instead webpack

--- packages/components/.storybook/main.mjs
+++ packages/components/.storybook/main.mjs
@@ -1,15 +1,13 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
-const config: StorybookConfig = {
+const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
---
+++ packages/components/vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'rhino-ui-components.js',
+      name: 'rhino-ui-components',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+});
